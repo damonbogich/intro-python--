f8a043531a49dc73c5b0a33904d40347fb8bca21
damonbogich <damonbogich@gmail.com>
2020-06-04 15:35:08 -0400

    got game working for moving player between rooms

--- src/adv.py
+++ src/adv.py
@@ -61,62 +61,105 @@ room['treasure'].s_to = room['narrow']
 #
 # If the user enters "q", quit the game.
 
-
-# def play_game(playerobj = damon):
-#     #make this in loop format:
-#     print(f'{playerobj.name} is in a room {playerobj.current_room}, which is {playerobj.current_room.description}')
-#     #direction prompt
-#     direction = input(f"enter direction you would like {playerobj.name} to move:")
-#     #if direction = 'n' run playerobj.current_room.n_to function
-#     if direction == 'n':
-#         print(playerobj.current_room.n_to) 
-
-
-# play_game()
-#new player: 
 damon = Player('Damon', room['outside'])
-# print(damon)
 
-# while (true):
-
-# player can move in 4 cardinal directions
-# game flow : You're a player who starts in a room, 
-# you can move from the room you're in to another 
-
-#function for game:
-def game(player=damon):
-    #looop
+def game(player= damon):
+    #make this in loop format:
     while(True):
-    #prints out current room name
-        
-        print(f'{player.current_room.name}')
-    #current room description
-        print(f'{player.current_room.description} ')
-    #waits for user input and decides what to do
+        #print current room name
+        # print((f'{player.current_room.name.s_to}')) --experiment with putting these in the class person later
+        print((f'{player.current_room.name}'))
+        #current room desc
+        print(f'{player.current_room.description}')
+        #waits for user input and decides what direction to move
         direction = input('Which way would you like to move?: ')
         print(f'current room:{player.current_room.name}, current direction: {direction} ')
-        #set up conditionals for each room/direction for user
-        #outisde(north), foyer(north, south, east), overlook(south), narrow(north, west), treasure(south)
+        #set conditionals allowing player to move from room to room
         if direction == 'q':
             break
-        #this elif prints every time anything other than q is set to direction
-        # elif direction != 'q' or direction != 'n' or direction != 'e' or direction != 'w' or direction != 's':
-        #     print('not a valid direction for this game')  
-
-        #tried the following as: if player.current_room.name == 'Outside Cave Entrance' or player.current_room.name == 'Foyer' and it did not work... why???
-        if player.current_room.name == 'Outside Cave Entrance'  and direction == 'n':
-            print('ran cave north function!')
+        elif player.current_room.name == 'Outside Cave Entrance'  and direction == 'n':
             player.current_room = player.current_room.n_to
+        elif player.current_room.name == 'Outside Cave Entrance' and direction == 'e':
+            print('can not move east from here')
+        elif player.current_room.name == 'Outside Cave Entrance' and direction == 'w':
+            print('can not move west from here')
+        elif player.current_room.name == 'Outside Cave Entrance' and direction == 's':
+            print('can not move south from here')
         elif player.current_room.name == 'Foyer' and direction == 'n':  
             player.current_room = player.current_room.n_to
         elif player.current_room.name == 'Foyer' and direction == 's':
             player.current_room = player.current_room.s_to
         elif player.current_room.name == 'Foyer' and direction == 'e':
             player.current_room = player.current_room.e_to
-        elif player.current_room.name == 'Outside Cave Entrance' and direction == 'e' or  direction == 'w' or direction == 's':
-            print('you must move north from here' )
         elif player.current_room.name == 'Foyer' and direction == 'w':
             print('you cannot move west from here')
+        elif player.current_room.name == 'Grand Overlook' and direction == 's':
+            player.current_room = player.current_room.s_to
+        elif player.current_room.name == 'Grand Overlook' and direction == 'n':
+            print('you cannot move north from here')
+        elif player.current_room.name == 'Grand Overlook' and direction == 'e':
+            print('you cannot move east from here')
+        elif player.current_room.name == 'Grand Overlook' and direction == 'w':
+            print('you cannot move west from here')
+        elif player.current_room.name == 'Narrow Passage' and direction == 'n':  
+            player.current_room = player.current_room.n_to
+        elif player.current_room.name == 'Narrow Passage' and direction == 'w':  
+            player.current_room = player.current_room.w_to
+        elif player.current_room.name == 'Narrow Passage' and direction == 'e':  
+            print('you cannot move east from here')
+        elif player.current_room.name == 'Narrow Passage' and direction == 's':  
+            print('you cannot move south from here')
+        elif player.current_room.name == 'Treasure Chamber' and direction == 's':  
+            player.current_room = player.current_room.s_to
+        elif player.current_room.name == 'Treasure Chamber' and direction == 'n':  
+            print('you cannot move north from here')
+        elif player.current_room.name == 'Treasure Chamber' and direction == 'e':  
+            print('you cannot move east from here')
+        elif player.current_room.name == 'Treasure Chamber' and direction == 'w':  
+            print('you cannot move west from here')
+        
+        
+        
+        
+        
+game()
+
+# player can move in 4 cardinal directions
+# game flow : You're a player who starts in a room, 
+# you can move from the room you're in to another 
+
+
+# damon = Player('Damon', room['outside'])
+
+# def game(player=damon):
+#     #looop
+#     while(True):
+#     #prints out current room name
+        
+#         print(f'{player.current_room.name}')
+#     #current room description
+#         print(f'{player.current_room.description} ')
+#     #waits for user input and decides what to do
+#         direction = input('Which way would you like to move?: ')
+#         print(f'current room:{player.current_room.name}, current direction: {direction} ')
+#         #set up conditionals for each room/direction for user
+#         #outisde(north), foyer(north, south, east), overlook(south), narrow(north, west), treasure(south)
+#         if direction == 'q':
+#             break
+#         if player.current_room.name == 'Outside Cave Entrance'  and direction == 'n':
+#             print('ran cave north function!')
+#             player.current_room = player.current_room.n_to
+#         elif player.current_room.name == 'Outside Cave Entrance' and direction == 'e' or  direction == 'w' or direction == 's':
+#             print('you must move north from here' )
+#         elif player.current_room.name == 'Foyer' and direction == 'n':  
+#             player.current_room = player.current_room.n_to
+#         elif player.current_room.name == 'Foyer' and direction == 's':
+#             player.current_room = player.current_room.s_to
+#         elif player.current_room.name == 'Foyer' and direction == 'e':
+#             player.current_room = player.current_room.e_to
+#         elif player.current_room.name == 'Foyer' and direction == 'w':
+#             print('you cannot move west from here')
+
         #set up foyer now:
         #if player.current_room.name == 'Foyer' and direction == 'n'.... add room foyer to line 105
             # player.current_room == player.current_room.n_to xxxx
@@ -128,6 +171,6 @@ def game(player=damon):
         #if player.current_room.name == 'Foyer' and direction == 'w'...
         #     print('you cannot move west from here')   
 
-        #foyer problems: whenever I set direction to w it prints 'you must move north from here'... should say 'cannot move west'
-        #No matter what I type when I'm in 'outside cave entrance' it sends me to foyer.  SHould only send me to foyer if i type 'n'
+        #foyer problems: south function returns line 111, direction set to w returns line 111
+        #tried the following as: if player.current_room.name == 'Outside Cave Entrance' or player.current_room.name == 'Foyer' and it did not work... why???
 game()
